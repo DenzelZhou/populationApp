53bb2b8b6fb0bf9de2a5d232edec46f8f9068aab
linan <linan.groot@outlook.com>
2017-03-21 21:36:34 +0800

    change data commit

--- User/js/save.js
+++ User/js/save.js
@@ -6,15 +6,18 @@
 }(mui, document));
 
 function submit(){
+	var data={
+		area:"1",
+		street:"2",
+		community:"3",
+		building:"4",
+		unit:"5"
+	};
 	mui.openWindow({
 		id: "userInfo",
 		url: "userInfo.html",
 		extras:{
-			area:"",
-			street:"",
-			community:"",
-			building:"",
-			unit:""
+			data
 		},
 	});
 }
